3f420b1effde8b4119d21ecb5b53449094f0dbfd
unknown <theophile.bornon@protonmail.com>
2018-11-14 22:37:05 +0100

    Test routes to remove when auth fully implemented

--- server/api/routes/authRoutes.js
+++ server/api/routes/authRoutes.js
@@ -6,7 +6,20 @@ module.exports = function (app) {
     app.use(auth.passport.initialize());
     app.use(auth.passport.session());
 
-    app.post('/api/login', auth.passport.authenticate('local'), (req,res) => {
+    app.post('/api/login', auth.passport.authenticate('login'), (req,res) => {
+        console.log(req.params);
+        res.send(req.user);
+    });
+
+    app.post('/api/test', auth.passport.authenticate('jwt'), (req,res,next) => {
+        req.test = "tzervezrvzerv";
+        next('error');
+    }, (req,res) => {
+        console.log(req.test);
+        res.send(req.user);
+    });
+
+    app.post('/api/partner', auth.passport.authenticate('jwt'), auth.areAuthorized(["Partner"]), (req,res) => {
         res.send(req.user);
     });
 };
