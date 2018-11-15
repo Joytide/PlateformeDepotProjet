41cb424fbcbb4fe486b027dd0be8e5e62d40f2c0
unknown <theophile.bornon@protonmail.com>
2018-11-15 20:17:52 +0100

    removal of video on home page

--- client/src/components/JumbotronPresentation.js
+++ client/src/components/JumbotronPresentation.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Jumbotron,Button,Row,Col, Container} from 'reactstrap';
+import { Jumbotron, Button, Row, Col, Container } from 'reactstrap';
 import Paper from 'material-ui/Paper';
 import i18n from './i18n';
 
 console.log("Passed");
 class JumbotronPresentation extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -13,28 +13,24 @@ class JumbotronPresentation extends React.Component {
         }
 
         this.style = {
-            video : {
-                height : 600+'px',
+            video: {
+                height: 600 + 'px',
             },
         }
-        
+
     }
 
-  
 
-	render() {
+
+    render() {
         const lng = this.props.lng;
 
-        return(
+        return (
             <Paper className="welcome"
-            rounded = {true}>
+                rounded={true}>
 
-                <h1  className="description display-3 text-center" lng={lng}>{i18n.t('welcomePlatform.h1', {lng})}</h1>
-                 <video style = {this.style.video} className="background-video embed-responsive" loop autoPlay>
-                    <source src={this.state.videoURL} type="video/mp4" />
-                    <source src={this.state.videoURL} type="video/ogg" />
-                    Your browser does not support the video tag.
-                 </video>
+                <h1 className="description display-3 text-center" lng={lng}>{i18n.t('welcomePlatform.h1', { lng })}</h1>
+
             </Paper>
         );
 
@@ -42,3 +38,11 @@ class JumbotronPresentation extends React.Component {
 }
 
 export default JumbotronPresentation;
+
+/*
+<video style = {this.style.video} className="background-video embed-responsive" loop autoPlay>
+    <source src={this.state.videoURL} type="video/mp4" />
+    <source src={this.state.videoURL} type="video/ogg" />
+    Your browser does not support the video tag.
+</video>
+*/
