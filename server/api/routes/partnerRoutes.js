e7520c688494c08957108285f92a74975840b7d3
BORNON Théophile <theophile.bornon@protonmail.com>
2018-12-04 09:29:35 +0100

    Verify partner id with regex + camel case function names

--- server/api/routes/partnerRoutes.js
+++ server/api/routes/partnerRoutes.js
@@ -4,12 +4,12 @@ const partner = require('../controllers/partnerController');
 
 module.exports = function (app) {
   app.route('/api/partners')
-    .get(partner.list_all_partners);
+    .get(partner.listPartners);
 
-  app.route('/api/partners/:partnerId')
-    .put(partner.update_a_partner)
-    .delete(partner.delete_a_partner);
+  app.route('/api/partners/:partnerId([a-fA-F0-9]{24})')
+    .post(partner.updatePartner)
+    .delete(partner.deletePartner);
 
   app.route('/api/partners/:email')
-    .get(partner.find_by_mail);
+    .get(partner.findByMail);
 };
