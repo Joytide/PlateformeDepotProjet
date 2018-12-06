76b25780f1b5720189483bf56a2b0e8203e6d204
unknown <theophile.bornon@protonmail.com>
2018-12-06 12:32:22 +0100

    Retrieve key for partner. Mail sending desactivated for easier dev

--- server/api/routes/mailsRoutes.js
+++ server/api/routes/mailsRoutes.js
@@ -3,9 +3,10 @@
 const mail = require('../controllers/mailController');
 
 module.exports = (app) => {
-    app.route('/api/mail')
-        .post(mail.sendMails);
+    // Impossible de laisser ça comme ça. Sinon tout le monde peut envoyer n'importe quoi depuis le compte mail
+    /*app.route('/api/mail')
+        .post(mail.sendMails);*/
 
-    app.route('/api/retrieveEdit')
+    app.route('/api/mail/recover')
         .post(mail.retrieveEdit);
 };
