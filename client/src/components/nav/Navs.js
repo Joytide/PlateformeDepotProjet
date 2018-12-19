45ffc66e57b956fb6153fa408c4781d6311b542f
smahot <steve.mahot@edu.devinci.fr>
2018-12-19 12:57:01 +0100

    Make AppBar responsive

--- client/src/components/nav/Navs.js
+++ client/src/components/nav/Navs.js
@@ -1,83 +1,219 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-import { Container, Row } from 'react-grid-system'
-import FlagIcon from 'react-flag-kit/lib/FlagIcon';
-import IconButton from 'material-ui/IconButton';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
 import i18n from '../i18n';
+import { Link } from 'react-router-dom';
 
-class Navs extends React.Component {
-	constructor(props) {
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class PrimaryAppBar extends React.Component {
+  
+  constructor(props) {
 		super(props)
 		this.styles = {
 			title: {
 				cursor: 'pointer',
 			},
-			menuItem: {
-				color: 'white',
-			},
 		};
 		this.state = {lng: 'en'}
-	}
+  }
+  
+  state = {
+    anchorEl: null,
+    mobileMoreAnchorEl: null,
+  };
 
-	handleclick(event) {
-		window.location.replace("/");
-	}
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
-	handleButtonClick(event) {
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+    this.handleMobileMenuClose();
+  };
 
-	}
+  handleMobileMenuOpen = event => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  };
 
-	render() {
-		let lng = this.props.lng;
+  handleMobileMenuClose = () => {
+    this.setState({ mobileMoreAnchorEl: null });
+  };
 
-		var configItem = [
-			{ label: i18n.t('home.label', {lng} ), href: "/" },
-			{ label: i18n.t('projects.label', {lng}), href: "/Projects" },
-			{ label: i18n.t('submit.label', {lng}), href: "/Deposit" },
-			{ label: i18n.t('login.label', {lng}), href: "/Connection" },
-			{ label: i18n.t('admin.label', {lng}), href: "/Admin" },
-			{ label: i18n.t('linkLost.label', {lng}), href: "/forgot" },
-			{ label: "FR", href: "#", icon: true },
-			{ label: "EN", href: "#", icon: true }
-		]
-		var menu = <Container><Row align='center'> {configItem.map((item) => {
-			if (item.icon) {
-				switch (item.label) {
-					case "FR":
-						return (<IconButton onClick={this.props.handleLngChange} className = "FR">
-							<FlagIcon code = "FR"/>
-						</IconButton>);
-					case "EN":
-						return (
-							<IconButton onClick={this.props.handleLngChange} className="EN">
-								<FlagIcon code = "GB"/>
-							</IconButton>);
-					default:
-							return (<div></div>);
-				}
+  render() {
+    const { anchorEl, mobileMoreAnchorEl } = this.state;
+    const { classes } = this.props;
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-			} else {
-				return <Link key={item.label} to={item.href}>
-					<FlatButton label={item.label} style={this.styles.menuItem} />
-				</Link>
-			}
-		})}
-		</Row>
-		</Container>
-		return (
-			<div>
-				<AppBar
-					title={
-					<Link to="/">
-						<img style={this.styles.title} src="/logo_pulv.png" height="50" width="50"/>
-					</Link>
-				}
-					iconElementRight={menu}
-					showMenuIconButton={false} />
-			</div>
-		);
-	}
+    let lng = this.props.lng;
+
+    const renderMenu = (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem component={Link} to="/Connection" color="inherit" className={classes.button}>
+            <div>{i18n.t('login.label', {lng} )}</div>
+        </MenuItem>
+        <MenuItem component={Link} to="/Admin" color="inherit" className={classes.button}>
+            <div>{i18n.t('admin.label', {lng} )}</div>
+        </MenuItem>
+        <MenuItem component={Link} to="/forgot" color="inherit" className={classes.button}>
+            <div>{i18n.t('linkLost.label', {lng} )}</div>
+        </MenuItem>
+      </Menu>
+    );
+
+    const renderMobileMenu = (
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={this.handleMobileMenuClose}
+      >
+        <MenuItem component={Link} to="/" color="inherit" className={classes.button}>
+            <div>{i18n.t('home.label', {lng} )}</div>
+        </MenuItem>
+        <MenuItem component={Link} to="/Projects" color="inherit" className={classes.button}>
+            <div>{i18n.t('projects.label', {lng} )}</div>
+        </MenuItem>
+        <MenuItem component={Link} to="/Deposit" color="inherit" className={classes.button}>
+            <div>{i18n.t('submit.label', {lng} )}</div>
+        </MenuItem>
+
+        <MenuItem onClick={this.handleProfileMenuOpen}>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </MenuItem>
+      </Menu>
+    );
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              Material-UI
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+
+              <Button component={Link} to="/" color="inherit" className={classes.button}>
+                <div>{i18n.t('home.label', {lng} )}</div>
+              </Button>
+              <Button component={Link} to="/Projects" color="inherit" className={classes.button}>
+                <div>{i18n.t('projects.label', {lng} )}</div>
+              </Button>
+              <Button component={Link} to="/Deposit" color="inherit" className={classes.button}>
+                <div>{i18n.t('submit.label', {lng} )}</div>
+              </Button>
+              
+              <IconButton
+                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        {renderMenu}
+        {renderMobileMenu}
+      </div>
+    );
+  }
 }
-export default Navs;
+
+PrimaryAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PrimaryAppBar);
