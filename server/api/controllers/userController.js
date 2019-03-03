e50cda95978e588ba1c988ef67695ddee3fbfaa8
BORNON Théophile <theophile.bornon@protonmail.com>
2019-03-03 10:20:51 +0100

    Error handling on user controller"

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -36,9 +36,9 @@ exports.listEPGE = (req, res) => {
         });
 }
 
-exports.create = (req, res) => {
+exports.create = (req, res, next) => {
     let data = req.body;
-
+    
     if (data.firstName && data.lastName && data.email && data.type) {
         if (data.type === "partner") {
             if (data.company) {
@@ -47,24 +47,27 @@ exports.create = (req, res) => {
                 partnerController
                     .createPartner(data)
                     .then(valid => res.send(valid))
-                    .catch(err => res.send(err));
+                    .catch(next);
             } else {
-                res.status(400).send(new Error(`Missing a parameter. Expected parameters : 
-                    (string) firstName, 
-                    (string) lastName, 
-                    (string) email, 
-                    ("student"|"partner"|"EPGE"|"administration") type
-                    (string) company (for partners only)`
-                ));
+                let error = new Error(`Missing a parameter. Expected parameters : 
+                (string) firstName, 
+                (string) lastName, 
+                (string) email, 
+                ("student"|"partner"|"EPGE"|"administration") type
+                (string) company (for partners only)`
+                );
+                error.status = 400;
+                next(error);
             }
         } else {
             Person.findOne({ email: data.email }, (err, person) => {
-                if (err) res.send(err);
+                if (err) next(err);
                 else {
                     if (person) {
                         let error = new Error("Email already used by a partner");
                         error.name = "EmailUsed";
-                        res.status(400).send(error);
+                        error.status = 400;
+                        next(error);
                     } else {
                         if (data.type === "EPGE" || data.type === "administration") {
                             if (data.password && data.password.length === 64) {
@@ -75,21 +78,26 @@ exports.create = (req, res) => {
                                 administration.EPGE = data.type === "EPGE";
                                 administration.admin = data.type | false;
 
+                                console.log(data.password, bcryptConf);
+
                                 bcrypt.hash(data.password, bcryptConf.saltRounds, (err, hash) => {
-                                    if (err) res.send(err);
+                                    if (err) next(err);
                                     else {
+                                        console.log("here");
                                         administration.password = hash;
 
                                         administration.save((err, ad) => {
-                                            if (err) res.send(err);
+                                            if (err) next(err);
                                             else res.json(ad);
                                         });
                                     }
                                 });
                             } else {
-                                res.status(400).send(new Error(`Invalid password parameter :
+                                let error = new Error(`Invalid password parameter :
                                 (string) password. Must be hashed in sha256`
-                                ));
+                                );
+                                error.status = 400;
+                                next(error);
                             }
                         } else {
                             let student = new Student();
@@ -98,7 +106,7 @@ exports.create = (req, res) => {
                             student.email = data.email;
 
                             student.save((err, stu) => {
-                                if (err) res.send(err);
+                                if (err) next(err);
                                 else res.json(stu);
                             });
                         }
@@ -107,14 +115,17 @@ exports.create = (req, res) => {
             });
         }
     } else {
-        res.status(400).send(new Error(`Missing a parameter. Expected parameters : 
-            (string) firstName, 
-            (string) lastName, 
-            (string) email, 
-            ("student"|"partner"|"EPGE"|"administration") type
-            (string) company (for partners only)
-            (string) password (for admin|EPGE only). Must be hashed in sha256`
-        ));
+        let error = new Error(`Missing a parameter. Expected parameters : 
+        (string) firstName, 
+        (string) lastName, 
+        (string) email, 
+        ("student"|"partner"|"EPGE"|"administration") type
+        (string) company (for partners only)
+        (string) password (for admin|EPGE only). Must be hashed in sha256`
+        );
+        error.status = 400;
+        error.name = "MissingParameter";
+        next(error);
     }
 }
 
