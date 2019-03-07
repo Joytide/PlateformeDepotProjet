7e8d7e750272fb116b59765783d2c25b57dce677
smahot <steve.mahot@edu.devinci.fr>
2019-03-07 18:08:16 +0100

    page home refaite et fix traduction

--- client/src/components/nav/Navs.js
+++ client/src/components/nav/Navs.js
@@ -13,221 +13,221 @@ import i18n from '../i18n';
 import { Link } from 'react-router-dom';
 
 const styles = theme => ({
-  root: {
-    width: '100%',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-  button: {
-    margin: theme.spacing.unit,
-    color: '#FFFFFF',
-  },
-  menuButton: {
-    color: '#FFFFFF',
-  },
-  languageButton: {
-    fontSize: 25,
-    color: '#FFFFFF',
-  },
-  languageButtonMobile: {
-    fontSize: 25,
-    color: '#000000',
-    }
+	root: {
+		width: '100%',
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+		},
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%',
+	},
+	inputInput: {
+		paddingTop: theme.spacing.unit,
+		paddingRight: theme.spacing.unit,
+		paddingBottom: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit * 10,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: 200,
+		},
+	},
+	sectionDesktop: {
+		display: 'none',
+		[theme.breakpoints.up('md')]: {
+			display: 'flex',
+		},
+	},
+	sectionMobile: {
+		display: 'flex',
+		[theme.breakpoints.up('md')]: {
+			display: 'none',
+		},
+	},
+	button: {
+		margin: theme.spacing.unit,
+		color: '#FFFFFF',
+	},
+	menuButton: {
+		color: '#FFFFFF',
+	},
+	languageButton: {
+		fontSize: 25,
+		color: '#FFFFFF',
+	},
+	languageButtonMobile: {
+		fontSize: 25,
+		color: '#000000',
+		}
 });
 
 class Navs extends React.Component {
-  constructor(props) {
-    super(props)
-    this.styles = {
-      title: {
-        cursor: 'pointer',
-      },
-    };
-    this.state = {
-      lng: 'en'
-    }
-  }
-  
-  state = {
-    anchorEl: null,
-    mobileMoreAnchorEl: null,
-  };
+	constructor(props) {
+		super(props)
+		this.styles = {
+			title: {
+				cursor: 'pointer',
+			},
+		};
+		this.state = {
+			lng: 'en'
+		}
+	}
+	
+	state = {
+		anchorEl: null,
+		mobileMoreAnchorEl: null,
+	};
 
-  handleProfileMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+	handleProfileMenuOpen = event => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-    this.handleMobileMenuClose();
-  };
+	handleMenuClose = () => {
+		this.setState({ anchorEl: null });
+		this.handleMobileMenuClose();
+	};
 
-  handleMobileMenuOpen = event => {
-    this.setState({ mobileMoreAnchorEl: event.currentTarget });
-  };
+	handleMobileMenuOpen = event => {
+		this.setState({ mobileMoreAnchorEl: event.currentTarget });
+	};
 
-  handleMobileMenuClose = () => {
-    this.setState({ mobileMoreAnchorEl: null });
-  };
+	handleMobileMenuClose = () => {
+		this.setState({ mobileMoreAnchorEl: null });
+	};
 
-  handleclick(event) {
-    window.location.replace("/");
-  }
-  
-  handleButtonClick(event) {
+	handleclick(event) {
+		window.location.replace("/");
+	}
+	
+	handleButtonClick(event) {
 
 	}
 
-  render() {
-    const { anchorEl, mobileMoreAnchorEl } = this.state;
-    const { classes } = this.props;
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+	render() {
+		const { anchorEl, mobileMoreAnchorEl } = this.state;
+		const { classes } = this.props;
+		const isMenuOpen = Boolean(anchorEl);
+		const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-    let lng = this.props.lng;
+		let lng = this.props.lng;
 
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem component={Link} to="/Connection" color="inherit">
-            <div>{i18n.t('navs.login', {lng} )}</div>
-        </MenuItem>
-        <MenuItem component={Link} to="/Admin" color="inherit">
-            <div>{i18n.t('navs.admin', {lng} )}</div>
-        </MenuItem>
-        <MenuItem component={Link} to="/forgot" color="inherit">
-            <div>{i18n.t('navs.linkLost', {lng} )}</div>
-        </MenuItem>
-      </Menu>
-    );
+		const renderMenu = (
+			<Menu
+				anchorEl={anchorEl}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+				transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+				open={isMenuOpen}
+				onClose={this.handleMenuClose}
+			>
+				<MenuItem component={Link} to="/Connection" color="inherit">
+						<div>{i18n.t('navs.login', {lng} )}</div>
+				</MenuItem>
+				<MenuItem component={Link} to="/Admin" color="inherit">
+						<div>{i18n.t('navs.admin', {lng} )}</div>
+				</MenuItem>
+				<MenuItem component={Link} to="/forgot" color="inherit">
+						<div>{i18n.t('navs.linkLost', {lng} )}</div>
+				</MenuItem>
+			</Menu>
+		);
 
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMobileMenuOpen}
-        onClose={this.handleMobileMenuClose}
-      >
-        <Link to="/">
-          <MenuItem color="inherit">
-              <div>{i18n.t('navs.home', {lng} )}</div>
-          </MenuItem>
-        </Link>
-        <Link to="/Projects">
-          <MenuItem color="inherit">
-              <div>{i18n.t('navs.projects', {lng} )}</div>
-          </MenuItem>
-        </Link>
-        <Link to="/deposit">
-          <MenuItem color="inherit">
-              <div>{i18n.t('navs.submit', {lng} )}</div>
-          </MenuItem>
-        </Link>
-        <MenuItem  color="inherit">
-          <IconButton onClick={this.props.handleLngChange}>
-              {lng === 'en' ? <img src="/fr_flag.png" height="24" width="32" alt="french flag"/>:<img src="/usuk_flag.png" height="24" width="32" alt="english flag"/>}
-          </IconButton>
-          <IconButton onClick={this.handleProfileMenuOpen} color="inherit">
-            <AccountCircle />
-          </IconButton>
-        </MenuItem>
-      </Menu>
-    );
+		const renderMobileMenu = (
+			<Menu
+				anchorEl={mobileMoreAnchorEl}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+				transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+				open={isMobileMenuOpen}
+				onClose={this.handleMobileMenuClose}
+			>
+				<Link to="/">
+					<MenuItem color="inherit">
+							<div>{i18n.t('navs.home', {lng} )}</div>
+					</MenuItem>
+				</Link>
+				<Link to="/Projects">
+					<MenuItem color="inherit">
+							<div>{i18n.t('navs.projects', {lng} )}</div>
+					</MenuItem>
+				</Link>
+				<Link to="/deposit">
+					<MenuItem color="inherit">
+							<div>{i18n.t('navs.submit', {lng} )}</div>
+					</MenuItem>
+				</Link>
+				<MenuItem  color="inherit">
+					<IconButton onClick={this.props.handleLngChange}>
+							{lng === 'en' ? <img src="/fr_flag.png" height="24" width="32" alt="french flag"/>:<img src="/usuk_flag.png" height="24" width="32" alt="english flag"/>}
+					</IconButton>
+					<IconButton onClick={this.handleProfileMenuOpen} color="inherit">
+						<AccountCircle />
+					</IconButton>
+				</MenuItem>
+			</Menu>
+		);
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="fixed" color='primary'>
-           <Toolbar>
-            <Link to="/">
-             <img style={this.styles.title} alt="logo PULV" src="/logo_pulv.png" height="50" width="50"/>
-            </Link>
-            <div className={classes.grow} />
-            <div className={classes.sectionDesktop}>
-              <Link to="/">
-                <Button color="inherit" className={classes.button}>
-                  <div>{i18n.t('navs.home', {lng} )}</div>
-                </Button>
-              </Link>
-              <Link to="/Projects">
-                <Button color="inherit" className={classes.button}>
-                  <div>{i18n.t('navs.projects', {lng} )}</div>
-                </Button>
-              </Link>
-              <Link to="/deposit">
-                <Button color="inherit" className={classes.button}>
-                  <div>{i18n.t('navs.submit', {lng} )}</div>
-                </Button>
-              </Link>
+		return (
+			<div className={classes.root}>
+				<AppBar position="fixed" color='primary'>
+					 <Toolbar>
+						<Link to="/">
+						 <img style={this.styles.title} alt="logo PULV" src="/logo_pulv.png" height="50" width="50"/>
+						</Link>
+						<div className={classes.grow} />
+						<div className={classes.sectionDesktop}>
+							<Link to="/">
+								<Button color="inherit" className={classes.button}>
+									<div>{i18n.t('navs.home', {lng} )}</div>
+								</Button>
+							</Link>
+							<Link to="/Projects">
+								<Button color="inherit" className={classes.button}>
+									<div>{i18n.t('navs.projects', {lng} )}</div>
+								</Button>
+							</Link>
+							<Link to="/deposit">
+								<Button color="inherit" className={classes.button}>
+									<div>{i18n.t('navs.submit', {lng} )}</div>
+								</Button>
+							</Link>
 
-              <IconButton onClick={this.props.handleLngChange}>
-                  {lng === 'en' ? <img src="/fr_flag.png" height="24" width="32" alt="french flag"/>:<img src="/usuk_flag.png" height="24" width="32" alt="english flag"/>}
-              </IconButton>
-              
-              <IconButton
-                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-                aria-haspopup="true"
-                onClick={this.handleProfileMenuOpen}
-                className={classes.menuButton}
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
-                <MoreIcon />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        {renderMenu}
-        {renderMobileMenu}
-      </div>
-    );
-  }
+							<IconButton onClick={this.props.handleLngChange}>
+									{lng === 'en' ? <img src="/fr_flag.png" height="24" width="32" alt="french flag"/>:<img src="/usuk_flag.png" height="24" width="32" alt="english flag"/>}
+							</IconButton>
+							
+							<IconButton
+								aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+								aria-haspopup="true"
+								onClick={this.handleProfileMenuOpen}
+								className={classes.menuButton}
+							>
+								<AccountCircle />
+							</IconButton>
+						</div>
+						<div className={classes.sectionMobile}>
+							<IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+								<MoreIcon />
+							</IconButton>
+						</div>
+					</Toolbar>
+				</AppBar>
+				{renderMenu}
+				{renderMobileMenu}
+			</div>
+		);
+	}
 }
 
 Navs.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Navs);
