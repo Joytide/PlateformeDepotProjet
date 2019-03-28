13a3893fa5fee17e0f344d3b7fbabf913935cff9
unknown <theophile.bornon@protonmail.com>
2019-03-28 15:53:26 +0100

    pending is now default state for a project

--- server/api/models/Project.js
+++ server/api/models/Project.js
@@ -29,7 +29,8 @@ const ProjectSchema = new Schema({
     media_files: Array, //(String)
     status: {
         type: String,
-        required: true
+        required: true,
+        default:"pending"
     },
     sub_date: {
         type: Date,
