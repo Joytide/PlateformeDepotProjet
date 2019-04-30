398ad711d9fb672f4b91865545190c11558a1a8e
unknown <theophile.bornon@protonmail.com>
2019-04-30 09:42:42 +0200

    No useless header Dashboard with contact info

--- admin/src/components/Header/Header.jsx
+++ admin/src/components/Header/Header.jsx
@@ -16,49 +16,52 @@ import Button from "components/CustomButtons/Button.jsx";
 import headerStyle from "assets/jss/material-dashboard-react/components/headerStyle.jsx";
 
 function Header({ ...props }) {
-  function makeBrand() {
-    var name;
-    props.routes.map((prop, key) => {
-      if (prop.path === props.location.pathname) {
-        name = prop.navbarName;
-      }
-      return null;
-    });
-    return name || "";
-  }
-  const { classes, color } = props;
-  const appBarClasses = classNames({
-    [" " + classes[color]]: color
-  });
-  return (
-    <AppBar className={classes.appBar + appBarClasses}>
-      <Toolbar className={classes.container}>
-        <div className={classes.flex}>
-          {/* Here we create navbar brand, based on route name */}
-          <Button color="transparent" href="#" className={classes.title}>
-            {makeBrand()}
-          </Button>
-        </div>
-        <Hidden smDown implementation="css">
-          <HeaderLinks />
-        </Hidden>
-        <Hidden mdUp implementation="css">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={props.handleDrawerToggle}
-          >
-            <Menu />
-          </IconButton>
-        </Hidden>
-      </Toolbar>
-    </AppBar>
-  );
+	function makeBrand() {
+		var name;
+		props.routes.map((prop, key) => {
+			if (prop.path === props.location.pathname) {
+				name = prop.navbarName;
+			}
+			return null;
+		});
+		return name || "";
+	}
+
+	const { classes, color } = props;
+	const appBarClasses = classNames({
+		[" " + classes[color]]: color
+	});
+	return (
+		<AppBar className={classes.appBar + appBarClasses}>
+			<Toolbar className={classes.container}>
+				<div className={classes.flex}>
+					{/* Here we create navbar brand, based on route name */}
+					<Button color="transparent" href="#" className={classes.title}>
+						{makeBrand()}
+					</Button>
+				</div>
+				{/*
+				<Hidden smDown implementation="css">
+					<HeaderLinks />
+				</Hidden>
+				<Hidden mdUp implementation="css">
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						onClick={props.handleDrawerToggle}
+					>
+						<Menu />
+					</IconButton>
+				</Hidden>
+				*/}
+			</Toolbar>
+		</AppBar>
+	);
 }
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-  color: PropTypes.oneOf(["primary", "info", "success", "warning", "danger"])
+	classes: PropTypes.object.isRequired,
+	color: PropTypes.oneOf(["primary", "info", "success", "warning", "danger"])
 };
 
 export default withStyles(headerStyle)(Header);
