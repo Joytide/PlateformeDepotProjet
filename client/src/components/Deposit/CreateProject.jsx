9b623328e3ceb9326343652de5857dba550c6bdf
BORNON Théophile <theophile.bornon@protonmail.com>
2019-06-11 11:45:33 +0200

    tooltip when you hover item and not only text

--- client/src/components/Deposit/CreateProject.jsx
+++ client/src/components/Deposit/CreateProject.jsx
@@ -207,18 +207,18 @@ class CreateProject extends React.Component {
                                 input={<Input id="select-multiple" />}
                             >
                                 {this.state.specializations.map(specialization => (
-                                    <MenuItem key={specialization._id} value={specialization._id}>
-                                        <Tooltip primary="Drafts"
-                                            disableFocusListener
-                                            disableTouchListener
-                                            TransitionComponent={Zoom}
-                                            title={lng === "fr" ? specialization.description.fr : specialization.description.en}
-                                        >
+                                    <Tooltip primary="Drafts"
+                                        disableFocusListener
+                                        disableTouchListener
+                                        TransitionComponent={Zoom}
+                                        title={lng === "fr" ? specialization.description.fr : specialization.description.en}
+                                    >
+                                        <MenuItem key={specialization._id} value={specialization._id}>
                                             <div>
                                                 {lng === "fr" ? specialization.name.fr : specialization.name.en}
                                             </div>
-                                        </Tooltip>
-                                    </MenuItem>
+                                        </MenuItem>
+                                    </Tooltip>
                                 ))}
                             </Select>
                         </FormControl>
