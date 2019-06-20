fcf23f9f9847411e71ada004a55b236d3a932295
BORNON Théophile <theophile.bornon@protonmail.com>
2019-06-20 10:18:45 +0200

    comments

--- server/api/routes/authRoutes.js
+++ server/api/routes/authRoutes.js
@@ -3,13 +3,15 @@
 const auth = require('../controllers/authController');
 
 module.exports = function (app) {
+    /**
+     * Return token partner depending on his key
+     */
     app.post('/api/login/partner/', auth.logPartner);
     
+    /**
+     * Connect user using email + password
+     */
     app.post('/api/login/', auth.passport.authenticate('login'), (req, res) => {
-        res.send(req.user);
-    });
-
-    app.post('/api/partner', auth.passport.authenticate('jwt'), auth.areAuthorized(["Partner"]), (req, res) => {
         res.send(req.user);
     });
 };
--- server/api/routes/partnerRoutes.js
+++ server/api/routes/partnerRoutes.js
@@ -6,9 +6,11 @@ const auth = require('../controllers/authController');
 
 module.exports = function (app) {
 	app.route('/api/partner')
-		// Staff access only
 		.get(auth.passport.authenticate('jwt'), partner.listAllPartners)
-		.put(partner.createPartner);
+		.put(partner.createPartner)
+		.post(auth.passport.authenticate('jwt'), auth.areAuthorized(["Partner"]), (req, res) => {
+			res.send(req.user);
+		});
 
 
 	app.route('/api/partner/:id([a-fA-F0-9]{24})')
@@ -18,7 +20,7 @@ module.exports = function (app) {
 
 	app.route('/api/partner/:key([a-zA-Z0-9]{16})')
 		.get(partner.findByKey);
-		
+
 	//Keep that route in last
 	app.route('/api/partner/:email')
 		.get(partner.findByMail);
