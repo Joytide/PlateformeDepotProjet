56ecdbd8d7d75c71ed1a859954dca6d1774c6fca
BORNON Théophile <theophile.bornon@protonmail.com>
2019-06-20 16:41:20 +0200

    upload file authorized to EPGE

--- server/api/routes/projectRoutes.js
+++ server/api/routes/projectRoutes.js
@@ -5,7 +5,7 @@ const auth = require('../controllers/authController');
 
 module.exports = function (app) {
 	app.route('/api/project/file')
-		.post(auth.passport.authenticate('jwt'), auth.areAuthorized("Partner"), project.upload.single("file"), project.uploadDone)
+		.post(auth.passport.authenticate('jwt'), auth.areAuthorized(["Partner", "EPGE"]), project.upload.single("file"), project.uploadDone)
 		.delete(auth.passport.authenticate('jwt'), auth.areAuthorized("EPGE"), project.deleteFile);
 
 	app.route('/api/project/file/:id([a-fA-F0-9]{24})')
