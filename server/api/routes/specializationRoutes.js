dc40032110101299c5e5b3266de51e29497c157f
unknown <theophile.bornon@protonmail.com>
2020-03-31 15:13:43 +0200

    Refactoring spe routes

--- server/api/routes/specializationRoutes.js
+++ server/api/routes/specializationRoutes.js
@@ -1,17 +1,18 @@
 const specializationController = require('../controllers/specializationController');
 const auth = require('../controllers/authController');
+const { handleRequest } = require('../../helpers/Request');
 
 module.exports = (app) => {
     app.route('/api/specialization/:_id([a-zA-Z0-9]{24})')
-        .get(specializationController.findById);
+        .get(handleRequest(specializationController.findById));
 
     app.route('/api/specialization')
-        .get(specializationController.list)
-        .put(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), specializationController.create)
-        .delete(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), specializationController.delete)
-        .post(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), specializationController.update);
+        .get(handleRequest(specializationController.list))
+        .put(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), handleRequest(specializationController.create))
+        .delete(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), handleRequest(specializationController.delete))
+        .post(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), handleRequest(specializationController.update));
 
     app.route('/api/specialization/referent')
-        .put(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), specializationController.addReferent)
-        .delete(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), specializationController.removeReferent);
+        .put(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), handleRequest(specializationController.addReferent))
+        .delete(auth.passport.authenticate('jwt'), auth.areAuthorized("Administrator"), handleRequest(specializationController.removeReferent));
 }
