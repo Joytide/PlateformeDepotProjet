d34e1669a380ec9f019f5d80eaa5aaa1eae883b0
unknown <theophile.bornon@protonmail.com>
2020-04-02 02:00:07 +0200

    remove function

--- server/api/controllers/mailController.js
+++ server/api/controllers/mailController.js
@@ -163,46 +163,6 @@ ESILV's projects management team`;
         content: mailContent
     });
 });
-/*
-exports.retrieveEdit = (req, res) => {
-    if (req.body.email != undefined && req.body.email != '') {
-        Partner.findOne({ 'email': req.body.email }, (err, partner) => {
-            if (err)
-                res.send(err);
-            else if (partner) {
-                const recipient = req.body.email;
-                const subject = "URL d'édition des projets soumis sur DeVinci Plateforme."
-                const link = `${config.client.protocol}://${config.client.hostname + (config.client.port != 80 && config.client.port != 443 ? ':' + config.client.port : '')}/Edit/${partner.key}`
-                const content = `
-                Bonjour ${partner.first_name} ${partner.last_name} (${partner.company}), \n
-                Pour modifier votre projet, veuillez contacter l'administration à l'adresse mail suivante : berengere.branchet@devinci.fr \n
-                L'équipe DVP
-                `
-                const mail = {
-                    from: config.mail.email,
-                    to: recipient,
-                    subject: subject,
-                    text: content // html content possible. ;)
-                }
-
-                res.send(mail);
-                smtpTransporter.sendMail(mail, (err, res) => {
-                    if (err) {
-                        smtpTransporter.close();
-                        response.send(err);
-                    } else {
-                        smtpTransporter.close();
-                        response.send('Mail sent');
-                    }
-                });
-            } else {
-                res.send(new Error('Not found'));
-            }
-        });
-    } else {
-        res.send(new Error('Missing email parameter'));
-    }
-};*/
 
 const sendMail = data => {
     return new Promise((resolve, reject) => {
