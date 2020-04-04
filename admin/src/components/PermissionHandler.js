6487300ef3f282b77d1ac825ffe0dd2b4ee72f6d
unknown <theophile.bornon@protonmail.com>
2020-04-04 15:01:38 +0200

    handle referent permission type

--- admin/src/components/PermissionHandler.js
+++ admin/src/components/PermissionHandler.js
@@ -1,5 +1,4 @@
-exports.hasPermission = (permission, user, project) => {
-    console.log(permission, user, typeof permission)
+exports.hasPermission = (permission, user, specializations) => {
     if (user.admin)
         return true;
     if (typeof permission === "string") {
@@ -7,7 +6,7 @@ exports.hasPermission = (permission, user, project) => {
             return true;
         else if (permission === "EGPE" && user.EPGE)
             return true;
-        else if (permission === "Referent" && project.referent.indexOf(user._id) !== -1)
+        else if (permission === "Referent" && hasReferent(specializations, user))
             return true;
 
         return false;
@@ -16,7 +15,7 @@ exports.hasPermission = (permission, user, project) => {
             return true;
         if (permission.indexOf("EGPE") !== -1 && user.EPGE)
             return true;
-        if (permission.indexOf("Referent") !== -1 && project.referent.indexOf(user._id) !== -1)
+        if (permission.indexOf("Referent") !== -1 && hasReferent(specializations, user))
             return true;
 
         return false;
@@ -24,4 +23,15 @@ exports.hasPermission = (permission, user, project) => {
         console.error("Unknown permission type", permission, user);
         return false;
     }
+}
+
+const hasReferent = (specializations, user) => {
+    // If referent array is populated, unpopulate it
+    if (specializations[0] && specializations[0].referent[0] && typeof specializations[0].referent[0] !== "string")
+        specializations = specializations.map(spe => { return { referent: spe.referent.map(r => r._id) } })
+
+    for (let i = 0; i < specializations.length; i++)
+        if (specializations[i].referent.indexOf(user._id) !== -1)
+            return true;
+    return false;
 }
