9e24b43ae043bd42a3ca26ba46bc308f406cca41
Joytide <joytide@tutanota.com>
2021-09-04 21:02:57 +0200

    Restored BrowserRouter to avoid fucked up paths in admin app

--- admin/src/index.js
+++ admin/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Redirect } from 'react-router'
 import { createBrowserHistory } from "history";
 import {
-	Router,
+	BrowserRouter as Router,
 	Route,
 	Switch
 } from 'react-router-dom'
