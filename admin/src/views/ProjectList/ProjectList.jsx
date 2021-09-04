741461d9c46774edfeb9eee7bc603967f25eae06
Joytide <joytide@tutanota.com>
2021-09-04 02:48:05 +0200

    Removed useless button

--- admin/src/views/ProjectList/ProjectList.jsx
+++ admin/src/views/ProjectList/ProjectList.jsx
@@ -427,7 +427,6 @@ class ProjectList extends React.Component {
                             
                             
                             <GridItem xs={12} sm={12} md={6}>
-                                <Button>test</Button>
                                 <Button
                                     color= {this.state.alphaSortProject ? "success" : "white"}
                                     onClick={this.handleChange("alphaSortProject")}
