2f411385e87f75e6bbafe2b944d31a55ccd6e983
Joytide <joytide@tutanota.com>
2022-05-22 19:56:19 +0200

    Fixed file upload max file limit without clearing dropzone array

--- client/src/components/Deposit/FormComponents/FilesInputs.js
+++ client/src/components/Deposit/FormComponents/FilesInputs.js
@@ -13,12 +13,14 @@ class FilesInputs extends React.Component {
 
         this.state = {
             files: [],
+            file_count: 0,
             errorCode: 0
         }
 
         this.onChange = this.onChange.bind(this);
         this.setFiles = this.props.setFiles || (() => { });
         this.OnFilesError = this.OnFilesError.bind(this);
+
     }
 
     OnFilesError(error, file) {
@@ -27,55 +29,62 @@ class FilesInputs extends React.Component {
 
     onChange(event) {
         const file = event[event.length - 1];
-    
-        if (file) {
-            var formData = new FormData();
-            formData.append("file", new Blob([file], { type: file.type }), file.name || 'file');
-
-            fetch(api.host + ":" + api.port + '/api/project/file', {
-                method: "POST",
-                headers: {
-                    "Authorization": AuthService.getToken()
-                },
-                body: formData
-            })
-                .then(res => {
-                    if (!res.ok)
-                        throw res;
-                    else
-                        return res.json();
-                })
-                .then(data => {
-                    this.setState(state => {
-                        file._id = data._id;
-                        const list = [...state.files, {
-                            name: file.name,
-                            type: file.type,
-                            _id: data._id
-                        }];
-
-                        this.setFiles(list);
-                        return { files: list };
-                    });
-                })
-                .catch(err => {
-                    const { lng } = this.props;
-                    console.error(err);
-                    this.props.snackbar.notification("error", i18n.t('errors.default', { lng }), 10000);
-                });
+        if (this.state.file_count>=3){
+            this.props.snackbar.notification("error", i18n.t('errors.tooManyFiles', { lng: this.props.lng }), 10000);
         }
         else{
-            switch(this.state.errorCode){
-                case 1:
-                    this.props.snackbar.notification("error", i18n.t('errors.invalidFile', { lng: this.props.lng }), 10000);
-                    break;
-                case 2:
-                    this.props.snackbar.notification("error", i18n.t('errors.largeFile', { lng: this.props.lng }), 10000);
-                    break;
-                default:
-                    this.props.snackbar.notification("error", i18n.t('errors.default', { lng: this.props.lng }), 10000);
-                    break;
-            };
+            if (file && this.state.errorCode === 0) {
+                var formData = new FormData();
+                formData.append("file", new Blob([file], { type: file.type }), file.name || 'file');
+
+                fetch(api.host + ":" + api.port + '/api/project/file', {
+                    method: "POST",
+                    headers: {
+                        "Authorization": AuthService.getToken()
+                    },
+                    body: formData
+                })
+                    .then(res => {
+                        if (!res.ok)
+                            throw res;
+                        else
+                            return res.json();
+                    })
+                    .then(data => {
+                        this.setState(state => {
+                            file._id = data._id;
+                            const list = [...state.files, {
+                                name: file.name,
+                                type: file.type,
+                                _id: data._id
+                            }];
+                            this.setFiles(list);
+                            return { files: list, file_count: this.state.file_count+1, errorCode: 0};
+                        });
+                    })
+                    .catch(err => {
+                        const { lng } = this.props;
+                        console.error(err);
+                        this.props.snackbar.notification("error", i18n.t('errors.default', { lng }), 10000);
+                    });
+            }
+            else{
+                switch(this.state.errorCode){
+                    case 1:
+                        this.props.snackbar.notification("error", i18n.t('errors.invalidFile', { lng: this.props.lng }), 10000);
+                        break;
+                    case 2:
+                        this.props.snackbar.notification("error", i18n.t('errors.largeFile', { lng: this.props.lng }), 10000);
+                        break;
+                    case 3:
+                        this.props.snackbar.notification("error", i18n.t('errors.tooManyFiles', { lng: this.props.lng }), 10000);
+                        break;
+                    default:
+                        this.props.snackbar.notification("error", i18n.t('errors.default', { lng: this.props.lng }), 10000);
+                        break;
+                };
+                this.setState({ errorCode: 0});
+            }
         }
     }
 
@@ -90,7 +99,8 @@ class FilesInputs extends React.Component {
             .then(res => {
                 if (res.ok)
                     this.setState({
-                        files: this.state.files.filter(f => f._id !== fileID)
+                        files: this.state.files.filter(f => f._id !== fileID),
+                        file_count: this.state.file_count <= 0 ? 0 : this.state.file_count-1
                     })
                 else
                     throw res;
@@ -113,8 +123,8 @@ class FilesInputs extends React.Component {
                         onChange={this.onChange}
                         onError={this.OnFilesError}
                         accepts={['image/*', 'application/pdf']}
-                        maxFiles={3}
-                        maxFileSize={10000000}
+                        // maxFiles={3} this didn't take into account the fact that we could delete some files
+                        maxFileSize={10000000}   // 10MB per file limits to 6gb for 200 partners with 3 files
                         minFileSize={0}
                         clickable
                     >
@@ -128,7 +138,7 @@ class FilesInputs extends React.Component {
                             <a key={index} className="justify-content-between file-add list-group-item list-group-item-action">
                                 <div>
                                     <p>{file.name}</p>
-                                    <button onClick={this.deleteFile(file._id)}>{i18n.t('delete.label', { lng })} </button>
+                                    <button type="button" onClick={this.deleteFile(file._id)}>{i18n.t('delete.label', { lng })} </button>
                                 </div>
                             </a>)
                     )
