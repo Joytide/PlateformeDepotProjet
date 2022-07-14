76f8edbde639ace85cca788317d72d9db8c51a25
Joytide <joytide@tutanota.com>
2022-07-15 00:43:47 +0200

    updated year to use correct db

--- server/api/helpers/mongo.js
+++ server/api/helpers/mongo.js
@@ -1,6 +1,6 @@
 const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME || "mongo";
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "esilv-projects-server-2021";
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "esilv-projects-server-2022";
 const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 // Construct MONGO connection URI depending on wether or not an username is passed to environment variables
